cd4f4f4c45ebe1a94e467851d54d666f1dea6d1d
Bcz25 <160162513+Bcz25@users.noreply.github.com>
2024-04-08 04:35:45 +0000

    updated js

--- assets/script.js
+++ assets/script.js
@@ -1,26 +1,19 @@
 const APIKey = "d819c0f02622027c482907b6666513c6";
 const submitBtn = document.getElementById('submitBtn');
 const currentCity = document.getElementById('current-search');
-const cityName = document.getElementById('city-input');
-const savedCity = localStorage.getItem('savedCity');
 const searchHistory = document.getElementById('search-history');
-
-function getAPI() {
-    const cityName = cityNameInput.value.trim();
-    const requestUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${APIKey}`;
-
-    fetch(requestUrl)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (search) {
-            console.log(search);
-        })
+const cityNameInput = document.getElementById('city-input');
+const savedCity = localStorage.getItem('savedCity');
+if (savedCity) {
+    cityNameInput.value = savedCity;
 }
 
 function recentSearches(){
-    console.log(savedCity)
+    const cityName = cityNameInput.value.trim();
+    console.log(cityName);
+
     localStorage.setItem('savedCity', cityName);
+
     const listItem = document.createElement('li');
     listItem.textContent = cityName;
     listItem.classList.add('prev-search');
